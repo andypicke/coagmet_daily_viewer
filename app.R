9b0c3d81ee4cef3eb514cd675451b876db010373
Andy <andypicke@gmail.com>
2024-08-05 14:46:16 -0600

    round rel hum values in table

--- app.R
+++ app.R
@@ -113,6 +113,8 @@ server <- function(input, output) {
     {
       data_merged() |>
         select(station, name, location, max_temp, min_temp, precip, solar_rad, rh_min, rh_max) |>
+        mutate(rh_min = round(rh_min, 2)) |>
+        mutate(rh_max = round(rh_max, 2)) |>
         datatable(
           rownames = FALSE,
           extensions = c("Responsive", "Buttons"),
